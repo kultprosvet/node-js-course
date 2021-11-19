312b8aa5b9698f1a285f693aa7e92d7b26011926
Stanislav Volnjanskij <stas@kultprosvet.net>
2021-11-19 10:10:59 +0200

    restored lesson 2 files

--- src/typeorm/lesson2/laptopRouter.ts
+++ src/typeorm/lesson2/laptopRouter.ts
@@ -5,6 +5,55 @@ import { Brand } from '../models/Brand'
 
 export const laptopRouter = express.Router()
 laptopRouter.get('/', async (req, res) => {
-  const data = await Laptop.find({ relations: ['promotions'] })
+  const data = await Laptop.find({
+    // relations: ['brand', 'screenSize', 'screenResolution', 'images'],
+    select: ['id', 'model'],
+    where: { brand: { id: In([1, 2]) } },
+  })
+  res.json(data)
+})
+laptopRouter.get('/group', async (req, res) => {
+  const data = await createQueryBuilder(Laptop, 'laptop')
+    .leftJoin('laptop.brand', 'brand')
+    .select('Min(price)', 'min')
+    .addSelect('Count(laptop.id)', 'count')
+    .addSelect('Max(price)', 'max')
+    .addSelect('Min(brandId)', 'brandId')
+    .addSelect('AVG(price)', 'avg')
+    .addSelect('Min(brand.name)', 'name')
+    .groupBy('laptop.brandId')
+    .getRawMany()
+  res.json(data)
+})
+laptopRouter.get('/detailsRaw', async (req, res) => {
+  const data = await createQueryBuilder(Laptop, 'laptop')
+    .leftJoinAndSelect('laptop.images', 'img')
+    .limit(2)
+    .offset(2)
+    .getRawMany()
+
+  res.json(data)
+})
+laptopRouter.get('/details', async (req, res) => {
+  const data = await createQueryBuilder(Laptop, 'laptop')
+    .leftJoinAndSelect('laptop.images', 'img')
+    .take(2)
+    .skip(1)
+    .getMany()
+
+  res.json(data)
+})
+laptopRouter.get('/sub', async (req, res) => {
+  const data = await createQueryBuilder(Laptop, 'laptop')
+    .select('id')
+    .addSelect((subQuery) => {
+      return subQuery
+        .from(Brand, 'brand')
+        .select('name')
+        .where('brand.id=laptop.brandId')
+    }, 'brandName')
+    .where(`id=:id`, { id: 1 })
+    .getRawMany()
+
   res.json(data)
 })
